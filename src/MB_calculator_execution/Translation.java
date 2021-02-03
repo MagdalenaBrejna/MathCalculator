c796f9a1af097278eee01d6e7ec0faeff0fbd75d
Magdalena Brejna <magdalena.brejna@wp.pl>
2021-02-03 21:12:31 +0100

    Creating the class Translation with method transforming expression to required form.

--- src/MB_calculator_execution/Translation.java
+++ src/MB_calculator_execution/Translation.java
@@ -1,4 +1,58 @@
 package MB_calculator_execution;
 
+import java.util.ArrayList;
+
 public class Translation {
+
+    public static ArrayList<String> translate(ArrayList<String> x)
+    {
+        ArrayList<String> wyr = new ArrayList<String>();
+        String a = "";
+        boolean blad = false;
+
+        for(int j = 0; j < x.size(); j++)
+        {
+            if((x.get(j).charAt(0) >= '0' && x.get(j).charAt(0) <= '9') || x.get(j).equals("."))
+                a += x.get(j);
+            else {
+                if(!(a.equals(""))) {
+                    if(a.equals("."))
+                        a = "0";
+                    else {
+                        int ile = 0;
+                        for(int k = 0; k < a.length(); k++)
+                            if(a.charAt(k) == '.')
+                                ile++;
+                        if(ile > 1)
+                            blad = true;
+                    }
+                    wyr.add(a);
+                    a = "";
+
+                } else if(x.get(j).equals("-"))
+                    a += x.get(j);
+
+                if(a == "")
+                    wyr.add(x.get(j));
+            }
+        }
+
+        if(!(a.equals(""))) {
+            if(a.equals("."))
+                a = "0";
+            else {
+                int ile = 0;
+                for(int k = 0; k < a.length(); k++)
+                    if(a.charAt(k) == '.')
+                        ile++;
+                if(ile > 1)
+                    blad = true;
+            }
+            wyr.add(a);
+            a = "";
+        }
+        if(blad)
+            wyr.clear();
+        return wyr;
+    }
 }
